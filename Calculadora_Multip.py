b2af860b3b4cee4e06f0278f0a39a1fbaaeb5d32
colddevblack <fabio_c_bordin@hotmail.com>
2018-05-09 06:15:55 -0300

    first commit

---
+++ Calculadora_Multip.py
@@ -0,0 +1,24 @@
+# Autor: Fabio de Campos Bordin - Junho 2015
+
+# -*- coding: utf-8 -*-
+
+print "Bem vindo a calculadora de multiplicacao "  
+""
+x = raw_input ("insira o primeiro numero para multiplicacao:")
+num1 = int(x)
+""
+y = raw_input ("insira o segundo numero para multiplicacao:")
+num2 =int(y)
+""
+
+
+print "Sua conta e",num1, " X ", num2  
+
+""
+
+print "Resultado ",num1*num2
+
+
+   
+
+
